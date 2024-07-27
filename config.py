1670927cc0fae1d44ad46d11e0de302a9f4d9c26
Neel321 <saptaparnadas331@gmail.com>
2024-07-27 13:42:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6680996149:AAEQQ4LNJXGmbIYc9Ga1TcO0pUJfysOq7Pw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("lol")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6600486066")
 
 
 
